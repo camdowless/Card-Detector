18b2e2d812b3f93f7087f5d6603649788d3c0b60
Cam Dowless <camdowless@gmail.com>
2020-06-18 13:16:31 -0400

    Create hull script Modify create_data

--- create_data.py
+++ create_data.py
@@ -1,5 +1,37 @@
+import hickle
 import cv2
 import os
+from glob import glob
+import random
+import hull as h
+
+
+card_suits=['s','h','d','c']
+card_values=['A','K','Q','J','10','9','8','7','6','5','4','3','2']
+
+
+
+class Cards():
+    """
+        Loads the 'cards.hkl' file into an instance of Card class
+    """
+    def __init__(self):
+        self._cards = hickle.load("cards.hkl")
+        # self.cards is a dictionary
+        # Keys: card names, Kc, 10s, 7d
+        # Values: lists of (img, hullHL, hullLR)
+
+        self._num_cards_by_value = {k : len(self._cards[k]) for k in self._cards}
+        print("No. of cards loaded per name: ", self._num_cards_by_value)
+
+    def get_random(self, card_name = None, display=False):
+        if card_name is None:
+            card_name = random.choice(list(self._cards.keys()))
+        card, hull1, hull2 = self._cards[card_name][random.randint(0, self._num_cards_by_value[card_name]-1)]
+        if display:
+            h.display_img(card, [hull1, hull2], "rgb")
+        return card,card_name,hull1,hull2
+
 
 def change_brightness(img, value):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -18,12 +50,96 @@ def change_brightness(img, value):
     img = cv2.cvtColor(final, cv2.COLOR_HSV2BGR)
     return img
 
-images = list()
+def edit_cards():
+    images = list()
 
-for image_string in os.listdir("cropped_cards"):
-    image = cv2.imread(f"cropped_cards/{image_string}")
-    for i in range(0, 100, 4):
-        bright_image = change_brightness(image, i)
-        dark_image = change_brightness(image, i*-1)
-        cv2.imwrite(f"edited_cards/{image_string}_{i}_b.JPG", bright_image)
-        cv2.imwrite(f"edited_cards/{image_string}_{i}_d.JPG", dark_image)
+    for image_string in os.listdir("cropped_cards"):
+        image = cv2.imread(f"cropped_cards/{image_string}")
+        for i in range(0, 100, 4):
+            image_string = image_string.replace(".JPG", "")
+            path = os.path.join("edited_cards", image_string)
+            print(path)
+            if not os.path.exists(path):
+                os.makedirs(path)
+            bright_image = change_brightness(image, i)
+            dark_image = change_brightness(image, i * -1)
+            bright_path = os.path.join(path, f"{image_string}_{i}_b.JPG")
+            dark_path = os.path.join(path, f"{image_string}_{i}_d.JPG")
+            cv2.imwrite(bright_path, bright_image)
+            cv2.imwrite(dark_path, dark_image)
+
+
+def saveToHickle():
+    imgs_dir = "edited_cards"
+    cards = {}
+    for suit in card_suits:
+        for value in card_values:
+            card_name = value+suit
+            card_dir = os.path.join(imgs_dir, card_name)
+            if not os.path.isdir(card_dir):
+                print(f"{card_dir} does not exist")
+                continue
+            cards[card_name] = []
+            for f in glob(card_dir + "/*.JPG"):
+                img = cv2.imread(f, cv2.IMREAD_UNCHANGED)
+                hullHL = h.findHull(img, h.refCornerHL, debug="no")
+                if hullHL is None:
+                    print(f"File {f} not used.")
+                    continue
+                hullLR = h.findHull(img, h.refCornerLR, debug="no")
+                if hullLR is None:
+                    print(f"File {f} not used.")
+                    continue
+                # Store the image in RBG format
+                # OpenCV is no longer needed
+                img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+                cards[card_name].append((img, hullHL, hullLR))
+            print(f"No. images for {card_name} : {len(cards[card_name])}")
+    print("Saved in: ", "cards.hkl")
+    hickle.dump(cards, "cards.hkl", mode='w')
+
+
+xml_body_1="""<annotation>
+        <folder>FOLDER</folder>
+        <filename>{FILENAME}</filename>
+        <path>{PATH}</path>
+        <source>
+                <database>Unknown</database>
+        </source>
+        <size>
+                <width>{WIDTH}</width>
+                <height>{HEIGHT}</height>
+                <depth>3</depth>
+        </size>
+"""
+xml_object=""" <object>
+                <name>{CLASS}</name>
+                <pose>Unspecified</pose>
+                <truncated>0</truncated>
+                <difficult>0</difficult>
+                <bndbox>
+                        <xmin>{XMIN}</xmin>
+                        <ymin>{YMIN}</ymin>
+                        <xmax>{XMAX}</xmax>
+                        <ymax>{YMAX}</ymax>
+                </bndbox>
+        </object>
+"""
+xml_body_2="""</annotation>        
+"""
+
+def create_voc_xml(xml_file, img_file, listbba, display=False):
+    with open(xml_file, "w") as f:
+        f.write(xml_body_1.format(**{'FILENAME':os.path.basename(img_file), 'PATH':img_file,'WIDTH':h.imgW,'HEIGHT':h.imgH}))
+        for bba in listbba:
+            f.write(xml_object.format(
+                **{'CLASS': bba.classname, 'XMIN': bba.x1, 'YMIN': bba.y1, 'XMAX': bba.x2, 'YMAX': bba.y2}))
+            f.write(xml_body_2)
+            if display:
+                print("New XML: ", xml_file)
+
+
+
+# saveToHickle()
+cards = Cards()
+_ = cards.get_random(display=True)
---
+++ hull.py
@@ -0,0 +1,189 @@
+import random
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import cv2
+from glob import glob
+import numpy as np
+
+
+cardW=64
+cardH=89
+cornerXmin=1.5
+cornerXmax=9.5
+cornerYmin=2.5
+cornerYmax=23
+
+zoom=4
+cardW*=zoom
+cardH*=zoom
+cornerXmin=int(cornerXmin*zoom)
+cornerXmax=int(cornerXmax*zoom)
+cornerYmin=int(cornerYmin*zoom)
+cornerYmax=int(cornerYmax*zoom)
+
+# imgW,imgH: dimensions of the generated dataset images
+imgW=720
+imgH=720
+
+refCard=np.array([[0,0],[cardW,0],[cardW,cardH],[0,cardH]],dtype=np.float32)
+refCardRot=np.array([[cardW,0],[cardW,cardH],[0,cardH],[0,0]],dtype=np.float32)
+refCornerHL=np.array([[cornerXmin,cornerYmin],[cornerXmax,cornerYmin],[cornerXmax,cornerYmax],[cornerXmin,cornerYmax]],dtype=np.float32)
+refCornerLR=np.array([[cardW-cornerXmax,cardH-cornerYmax],[cardW-cornerXmin,cardH-cornerYmax],[cardW-cornerXmin,cardH-cornerYmin],[cardW-cornerXmax,cardH-cornerYmin]],dtype=np.float32)
+refCorners=np.array([refCornerHL,refCornerLR])
+
+
+
+def findHull(img, corner = refCornerHL, debug = False):
+    """
+            Find in the zone 'corner' of image 'img' and return, the convex hull delimiting
+            the value and suit symbols
+            'corner' (shape (4,2)) is an array of 4 points delimiting a rectangular zone,
+            takes one of the 2 possible values : refCornerHL or refCornerLR
+            debug=
+        """
+
+    kernel = np.ones((3,3), np.uint8)
+    corner = corner.astype(np.int)
+
+    x1 = int(corner[0][0])
+    y1 = int(corner[0][1])
+    x2 = int(corner[2][0])
+    y2 = int(corner[2][1])
+    w = x2 - x1
+    h = y2 - y1
+    zone = img[y1:y2, x1:x2].copy()
+
+    strange_cnt = np.zeros_like(zone)
+    gray = cv2.cvtColor(zone, cv2.COLOR_BGR2GRAY)
+    thld = cv2.Canny(gray, 30, 200)
+    thld = cv2.dilate(thld, kernel, iterations=1)
+    if debug != "no":
+        cv2.imshow("thld", thld)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    # Find contours
+    contours, _ = cv2.findContours(thld.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    min_area = 30 # Reject contours with a small area. TWEAK 'zoom' dependent
+    min_solidity = 0.3 # reject contours with a low solidity TWEAK
+
+    concat_contour = None
+    ok = True
+    for c in contours:
+        area = cv2.contourArea(c)
+
+        hull = cv2.convexHull(c)
+        hull_area = cv2.contourArea(hull)
+        solidity = float(area) / hull_area
+        # Determine the center of gravity (cx,cy) of the contour
+        M = cv2.moments(c)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        #  abs(w/2-cx)<w*0.3 and abs(h/2-cy)<h*0.4 : TWEAK, the idea here is to keep only the contours which are closed to the center of the zone
+        if area >= min_area and abs(w / 2 - cx) < w * 0.3 and abs(h / 2 - cy) < h * 0.4 and solidity > min_solidity:
+            if debug != "no":
+                cv2.drawContours(zone, [c], 0, (255, 0, 0), -1)
+            if concat_contour is None:
+                concat_contour = c
+            else:
+                concat_contour = np.concatenate((concat_contour, c))
+        if debug != "no" and solidity <= min_solidity:
+            print("Solidity", solidity)
+            cv2.drawContours(strange_cnt, [c], 0, 255, 2)
+            cv2.imshow("Strange contours", strange_cnt)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+    if concat_contour is not None:
+        # At this point, we suppose that 'concat_contour' contains only the contours corresponding the value and suit symbols
+        # We can now determine the hull
+        hull = cv2.convexHull(concat_contour)
+        hull_area = cv2.contourArea(hull)
+        # If the area of the hull is to small or too big, there may be a problem
+        min_hull_area = 940  # TWEAK, deck and 'zoom' dependant
+        max_hull_area = 2120  # TWEAK, deck and 'zoom' dependant
+        if hull_area < min_hull_area or hull_area > max_hull_area:
+            ok = False
+            if debug != "no":
+                print("Hull area=", hull_area, "too large or too small")
+        # So far, the coordinates of the hull are relative to 'zone'
+        # We need the coordinates relative to the image -> 'hull_in_img'
+        hull_in_img = hull + corner[0]
+
+    else:
+        ok = False
+
+    if debug != "no":
+        if concat_contour is not None:
+            cv2.drawContours(zone, [hull], 0, (0, 255, 0), 1)
+            cv2.drawContours(img, [hull_in_img], 0, (0, 255, 0), 1)
+        cv2.imshow("Zone", zone)
+        cv2.imshow("Image", img)
+        if ok and debug != "pause_always":
+            key = cv2.waitKey(1)
+        else:
+            key = cv2.waitKey(0)
+        if key == 27:
+            return None
+    if ok == False:
+        return None
+
+    return hull_in_img
+
+
+def display_img(img, polygons=None, channels="bgr", size=9):
+    """
+        Function to display an inline image, and draw optional polygons (bounding boxes, convex hulls) on it.
+        Use the param 'channels' to specify the order of the channels ("bgr" for an image coming from OpenCV world)
+    """
+    if polygons is None:
+        polygons = []
+    if not isinstance(polygons,list):
+        polygons=[polygons]
+    if channels=="bgr": # bgr (cv2 image)
+        nb_channels=img.shape[2]
+        if nb_channels==4:
+            img=cv2.cvtColor(img,cv2.COLOR_BGRA2RGBA)
+        else:
+            img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    fig,ax=plt.subplots(figsize=(size,size))
+    ax.set_facecolor((0,0,0))
+    ax.imshow(img)
+
+
+    for polygon in polygons:
+        # An polygon has either shape (n,2),
+        # either (n,1,2) if it is a cv2 contour (like convex hull).
+        # In the latter case, reshape in (n,2)
+        if len(polygon.shape)==3:
+            polygon=polygon.reshape(-1,2)
+        patch=patches.Polygon(polygon,linewidth=1,edgecolor='g',facecolor='none')
+        ax.add_patch(patch)
+
+
+
+def testDimensions():
+
+    """
+        These 5 lines take 5 random cards and draw a box around the corners.
+        Used to test the dimensions CardW, CardH, CornerXMin, CornerXMax, CornerYMin, and CornerYMax
+    """
+    imgs_dir = "edited_cards"
+    imgs_fns = glob(imgs_dir + "/*.JPG")
+    for _ in range(0, 10):
+        img_fn = random.choice(imgs_fns)
+        display_img(cv2.imread(img_fn, cv2.IMREAD_UNCHANGED), polygons=[refCornerHL, refCornerLR])
+
+
+def testHull():
+    debug="no"
+    imgs_dir="edited_cards"
+    imgs_fns=glob(imgs_dir+"/*.JPG")
+    img_fn=random.choice(imgs_fns)
+    print(img_fn)
+    img=cv2.imread(img_fn,cv2.IMREAD_UNCHANGED)
+
+    hullHL=findHull(img,refCornerHL,debug=debug)
+    hullLR=findHull(img,refCornerLR,debug=debug)
+    display_img(img,[refCornerHL,refCornerLR,hullHL,hullLR])
+
+
